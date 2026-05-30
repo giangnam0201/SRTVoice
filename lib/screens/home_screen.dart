1d0f82a54bdff4132bf9522f081903b22dd28770
giangnam0201 <giangnam0201@users.noreply.github.com>
2026-05-30 09:30:34 +0000

    fix: Remove unused import in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import '../models/subtitle_entry.dart';
