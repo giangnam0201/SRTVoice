9359748626d0c9133e459d8e4fceb2b73daefbf7
giangnam0201 <giangnam0201@users.noreply.github.com>
2026-05-31 13:05:26 +0000

    deploy: 0db90435b6daef10aaa27e5d757d9aac9bea61d5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3917174753"
+    serviceWorkerVersion: "2462043106"
   }
 });
